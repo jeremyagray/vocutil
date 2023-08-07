f8a440f1df13e4f3a9a2858f24d89b2048de0135
Jeremy A Gray <jeremy.a.gray@gmail.com>
2023-08-07 13:46:23 -0500

    feat(vocutil): inherit from item
    Inherit from class Item to create FillInTheBlank.
    
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- vocutil/cc/fib.py
+++ vocutil/cc/fib.py
@@ -12,16 +12,21 @@
 
 """Common Cartridge fill-in-the-blank item."""
 
-import uuid
 import xml.etree.ElementTree as ET
 
+from .item import Item
 
-class FillInTheBlank:
+
+class FillInTheBlank(Item):
     """A Common Cartridge fill in the blank item."""
 
-    def __init__(self, qdata, **kwargs):
+    def __init__(self, question, answers, case="No", **kwargs):
         """Initialize a fill in the item."""
-        self.uuid = uuid.uuid4()
+        super().__init__(**kwargs)
+        self.question = question
+        self.answers = answers
+        self.case = case if case == "Yes" else "No"
+
         self.item = ET.Element("item", ident=str(self.uuid))
         self.itemmetadata = ET.SubElement(self.item, "itemmetadata")
         self.qtimetadata = ET.SubElement(self.itemmetadata, "qtimetadata")
@@ -33,7 +38,7 @@ class FillInTheBlank:
         self.presentation = ET.SubElement(self.item, "presentation")
         self.material = ET.SubElement(self.presentation, "material")
         self.mattext = ET.SubElement(self.material, "mattext", texttype="text/html")
-        self.mattext.text = qdata["question"]
+        self.mattext.text = self.question
 
         self.response = ET.SubElement(
             self.presentation,
@@ -63,11 +68,11 @@ class FillInTheBlank:
         condvar = ET.SubElement(cond, "conditionvar")
 
         # Set possible correct answers.
-        for answer in qdata["answers"]:
+        for answer in self.answers:
             varequal = ET.SubElement(
                 condvar,
                 "varequal",
-                case=qdata["case"] if qdata["case"] == "Yes" else "No",
+                case=self.case,
                 respident=f"fib-resp-{str(self.uuid)}",
             )
             varequal.text = answer
@@ -76,3 +81,60 @@ class FillInTheBlank:
         setvar.text = "100"
 
         return
+
+    def to_xml(self):
+        """Initialize a fill in the item."""
+        item = ET.Element("item", ident=str(self.uuid))
+        itemmetadata = ET.SubElement(item, "itemmetadata")
+        qtimetadata = ET.SubElement(itemmetadata, "qtimetadata")
+        qtimetadatafield = ET.SubElement(qtimetadata, "qtimetadatafield")
+        fieldlabel = ET.SubElement(qtimetadatafield, "fieldlabel")
+        fieldlabel.text = "cc_profile"
+        fieldentry = ET.SubElement(qtimetadatafield, "fieldentry")
+        fieldentry.text = "cc.fib.v0p1"
+        presentation = ET.SubElement(item, "presentation")
+        material = ET.SubElement(presentation, "material")
+        mattext = ET.SubElement(material, "mattext", texttype="text/html")
+        mattext.text = self.question
+
+        response = ET.SubElement(
+            presentation,
+            "response_str",
+            rcardinality="Single",
+            ident=f"fib-resp-{str(self.uuid)}",
+        )
+        blank = ET.SubElement(response, "render_fib", prompt="Dashline")
+
+        grade = ET.SubElement(item, "resprocessing")
+        outcomes = ET.SubElement(grade, "outcomes")
+        decvar = ET.SubElement(
+            outcomes,
+            "decvar",
+            maxvalue="100",
+            minvalue="0",
+            varname="SCORE",
+            vartype="Decimal",
+        )
+        cond = ET.SubElement(
+            grade,
+            "respcondition",
+            attrib={
+                "continue": "No",
+            },
+        )
+        condvar = ET.SubElement(cond, "conditionvar")
+
+        # Set possible correct answers.
+        for answer in self.answers:
+            varequal = ET.SubElement(
+                condvar,
+                "varequal",
+                case=self.case,
+                respident=f"fib-resp-{str(self.uuid)}",
+            )
+            varequal.text = answer
+
+        setvar = ET.SubElement(cond, "setvar", action="Set", varname="SCORE")
+        setvar.text = "100"
+
+        return ET.tostring(item, encoding="unicode")
--- vocutil/cc/test_fib.py
+++ vocutil/cc/test_fib.py
@@ -32,14 +32,13 @@ def test_one_correct_response_case_insensitive():
         ],
     }
 
-    item = vocutil.cc.FillInTheBlank(qdata)
+    item = vocutil.cc.FillInTheBlank(
+        qdata["question"], qdata["answers"], case=qdata["case"]
+    )
 
     actual = f"""<item ident="{str(item.uuid)}"><itemmetadata><qtimetadata><qtimetadatafield><fieldlabel>cc_profile</fieldlabel><fieldentry>cc.fib.v0p1</fieldentry></qtimetadatafield></qtimetadata></itemmetadata><presentation><material><mattext texttype="text/html">&lt;p&gt;_:  the process of separating a wave of different frequencies into its individual component waves&lt;/p&gt;</mattext></material><response_str rcardinality="Single" ident="fib-resp-{str(item.uuid)}"><render_fib prompt="Dashline" /></response_str></presentation><resprocessing><outcomes><decvar maxvalue="100" minvalue="0" varname="SCORE" vartype="Decimal" /></outcomes><respcondition continue="No"><conditionvar><varequal case="No" respident="fib-resp-{str(item.uuid)}">dispersion</varequal></conditionvar><setvar action="Set" varname="SCORE">100</setvar></respcondition></resprocessing></item>"""
 
-    expected = ET.tostring(
-        item.item,
-        encoding="unicode",
-    )
+    expected = item.to_xml()
 
     assert actual == expected
 
@@ -54,14 +53,13 @@ def test_one_correct_response_case_sensitive():
         ],
     }
 
-    item = vocutil.cc.FillInTheBlank(qdata)
+    item = vocutil.cc.FillInTheBlank(
+        qdata["question"], qdata["answers"], case=qdata["case"]
+    )
 
     actual = f"""<item ident="{str(item.uuid)}"><itemmetadata><qtimetadata><qtimetadatafield><fieldlabel>cc_profile</fieldlabel><fieldentry>cc.fib.v0p1</fieldentry></qtimetadatafield></qtimetadata></itemmetadata><presentation><material><mattext texttype="text/html">&lt;p&gt;_:  the process of separating a wave of different frequencies into its individual component waves&lt;/p&gt;</mattext></material><response_str rcardinality="Single" ident="fib-resp-{str(item.uuid)}"><render_fib prompt="Dashline" /></response_str></presentation><resprocessing><outcomes><decvar maxvalue="100" minvalue="0" varname="SCORE" vartype="Decimal" /></outcomes><respcondition continue="No"><conditionvar><varequal case="Yes" respident="fib-resp-{str(item.uuid)}">dispersion</varequal></conditionvar><setvar action="Set" varname="SCORE">100</setvar></respcondition></resprocessing></item>"""
 
-    expected = ET.tostring(
-        item.item,
-        encoding="unicode",
-    )
+    expected = item.to_xml()
 
     assert actual == expected
 
@@ -77,14 +75,13 @@ def test_two_correct_responses_case_insensitive():
         ],
     }
 
-    item = vocutil.cc.FillInTheBlank(qdata)
+    item = vocutil.cc.FillInTheBlank(
+        qdata["question"], qdata["answers"], case=qdata["case"]
+    )
 
     actual = f"""<item ident="{str(item.uuid)}"><itemmetadata><qtimetadata><qtimetadatafield><fieldlabel>cc_profile</fieldlabel><fieldentry>cc.fib.v0p1</fieldentry></qtimetadatafield></qtimetadata></itemmetadata><presentation><material><mattext texttype="text/html">&lt;p&gt;_:  the process of separating a wave of different frequencies into its individual component waves&lt;/p&gt;</mattext></material><response_str rcardinality="Single" ident="fib-resp-{str(item.uuid)}"><render_fib prompt="Dashline" /></response_str></presentation><resprocessing><outcomes><decvar maxvalue="100" minvalue="0" varname="SCORE" vartype="Decimal" /></outcomes><respcondition continue="No"><conditionvar><varequal case="No" respident="fib-resp-{str(item.uuid)}">dispersion</varequal><varequal case="No" respident="fib-resp-{str(item.uuid)}">dispersions</varequal></conditionvar><setvar action="Set" varname="SCORE">100</setvar></respcondition></resprocessing></item>"""
 
-    expected = ET.tostring(
-        item.item,
-        encoding="unicode",
-    )
+    expected = item.to_xml()
 
     assert actual == expected
 
@@ -100,13 +97,12 @@ def test_two_correct_responses_case_sensitive():
         ],
     }
 
-    item = vocutil.cc.FillInTheBlank(qdata)
+    item = vocutil.cc.FillInTheBlank(
+        qdata["question"], qdata["answers"], case=qdata["case"]
+    )
 
     actual = f"""<item ident="{str(item.uuid)}"><itemmetadata><qtimetadata><qtimetadatafield><fieldlabel>cc_profile</fieldlabel><fieldentry>cc.fib.v0p1</fieldentry></qtimetadatafield></qtimetadata></itemmetadata><presentation><material><mattext texttype="text/html">&lt;p&gt;_:  the process of separating a wave of different frequencies into its individual component waves&lt;/p&gt;</mattext></material><response_str rcardinality="Single" ident="fib-resp-{str(item.uuid)}"><render_fib prompt="Dashline" /></response_str></presentation><resprocessing><outcomes><decvar maxvalue="100" minvalue="0" varname="SCORE" vartype="Decimal" /></outcomes><respcondition continue="No"><conditionvar><varequal case="Yes" respident="fib-resp-{str(item.uuid)}">dispersion</varequal><varequal case="Yes" respident="fib-resp-{str(item.uuid)}">dispersions</varequal></conditionvar><setvar action="Set" varname="SCORE">100</setvar></respcondition></resprocessing></item>"""
 
-    expected = ET.tostring(
-        item.item,
-        encoding="unicode",
-    )
+    expected = item.to_xml()
 
     assert actual == expected

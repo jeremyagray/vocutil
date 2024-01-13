7c6dadfbf70e87b9811111479901b5a13d32c4db
Jeremy A Gray <jeremy.a.gray@gmail.com>
2024-01-13 10:01:38 -0600

    style: update file banners
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- vocutil/test_load.py
+++ vocutil/test_load.py
@@ -2,7 +2,7 @@
 #
 # vocutil, educational vocabulary utilities.
 #
-# Copyright 2022 Jeremy A Gray <gray@flyquackswim.com>.
+# Copyright 2022-2024 Jeremy A Gray <gray@flyquackswim.com>.
 #
 # All rights reserved.
 #

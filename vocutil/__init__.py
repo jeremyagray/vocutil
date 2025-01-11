47f27bb06587ea42a1ab47771eab67826feaa41c
Jeremy A Gray <gray@flyquackswim.com>
2025-01-11 15:22:23 -0600

    fix(vocutil): remove TestGen specific code
    Remove some TestGen format specific code to courses that actually use
    those resources.
    
    Signed-off-by: Jeremy A Gray <gray@flyquackswim.com>

--- vocutil/__init__.py
+++ vocutil/__init__.py
@@ -2,7 +2,7 @@
 #
 # vocutil, educational vocabulary utilities.
 #
-# Copyright 2022-2024 Jeremy A Gray <gray@flyquackswim.com>.
+# Copyright 2022-2025 Jeremy A Gray <gray@flyquackswim.com>.
 #
 # All rights reserved.
 #
@@ -12,6 +12,8 @@
 
 """vocutil module initialization."""
 
+__version__ = "0.0.5"
+
 from . import cc
 from .create import create_unit
 from .enrich import enrich
@@ -20,8 +22,5 @@ from .load import _clean_glossary
 from .load import _load_glossary_csv
 from .load import _load_glossary_json
 from .load import load_glossary
-from .testgen import _load_testgen_file
-from .testgen import _parse_testgen_mc
-from .testgen import load_testgen_mc
 from .words import Entry
 from .words import Glossary

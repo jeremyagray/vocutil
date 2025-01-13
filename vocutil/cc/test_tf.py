29de1664df676edbd9ac42d4db6921bc1558aa04
Jeremy A Gray <gray@flyquackswim.com>
2025-01-12 19:14:49 -0600

    feat(vocutil): implement true/false item
    Implement true/false items with the `Item` interface.
    
    Github-closes: jeremyagray/vocutil#11
    Signed-off-by: Jeremy A Gray <gray@flyquackswim.com>

---
+++ vocutil/cc/test_tf.py
@@ -0,0 +1,77 @@
+# ******************************************************************************
+#
+# vocutil, educational vocabulary utilities.
+#
+# Copyright 2022-2025 Jeremy A Gray <gray@flyquackswim.com>.
+#
+# All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# ******************************************************************************
+
+"""vocutil CC multiple choice tests."""
+
+import json
+
+import defusedxml.ElementTree as ET
+
+import vocutil
+
+
+def test_simple_tf():
+    """Should produce a simple true/false item."""
+    qdata = {
+        "question": "<p>One is one more than zero.</p>",
+        "answer": True,
+    }
+
+    item = vocutil.cc.TrueFalse(qdata["question"], qdata["answer"])
+
+    actual = f"""<item ident="{str(item.uuid)}"><itemmetadata><qtimetadata><qtimetadatafield><fieldlabel>cc_profile</fieldlabel><fieldentry>cc.true_false.v0p1</fieldentry></qtimetadatafield></qtimetadata></itemmetadata><presentation><material><mattext texttype="text/html">&lt;p&gt;One is one more than zero.&lt;/p&gt;</mattext></material><response_lid ident="{str(item.uuid)}" rcardinality="Single"><render_choice><response_label ident="{str(item.uuid)}-01"><material><mattext texttype="text/plain">True</mattext></material></response_label><response_label ident="{str(item.uuid)}-02"><material><mattext texttype="text/plain">False</mattext></material></response_label></render_choice></response_lid></presentation><resprocessing><outcomes><decvar maxvalue="100" minvalue="0" varname="SCORE" vartype="Decimal" /></outcomes><respcondition continue="No"><conditionvar><varequal respident="{str(item.uuid)}">{str(item.uuid)}-01</varequal></conditionvar><setvar action="Set" varname="SCORE">100</setvar></respcondition></resprocessing></item>"""  # noqa: E501
+
+    expected = ET.tostring(
+        item.item,
+        encoding="unicode",
+    )
+
+    assert actual == expected
+
+
+def test_tf_init_should_roundtrip_json():
+    """Should round-trip with JSON ``__init__``."""
+    qdata = {
+        "question": "<p>One is one more than zero.</p>",
+        "answer": True,
+    }
+
+    item = vocutil.cc.TrueFalse(qdata["question"], qdata["answer"])
+
+    assert item.to_json() == json.dumps(qdata)
+
+
+def test_tf_cls_should_roundtrip_json():
+    """Should round-trip with JSON via class method."""
+    qdata = {
+        "question": "<p>One is one more than zero.</p>",
+        "answer": True,
+    }
+
+    item = vocutil.cc.TrueFalse.from_json(json.dumps(qdata))
+
+    assert item.to_json() == json.dumps(qdata)
+
+
+def test_tf_init_should_roundtrip_xml():
+    """Should round-trip with XML."""
+    qdata = {
+        "question": "<p>One is one more than zero.</p>",
+        "answer": True,
+    }
+
+    item = vocutil.cc.TrueFalse(qdata["question"], qdata["answer"])
+
+    xml_str = item.to_xml()
+    xml_item = vocutil.cc.TrueFalse.from_xml(xml_str)
+
+    assert item.question == xml_item.question
--- vocutil/cc/tf.py
+++ vocutil/cc/tf.py
@@ -2,7 +2,7 @@
 #
 # vocutil, educational vocabulary utilities.
 #
-# Copyright 2022-2024 Jeremy A Gray <gray@flyquackswim.com>.
+# Copyright 2022-2025 Jeremy A Gray <gray@flyquackswim.com>.
 #
 # All rights reserved.
 #
@@ -11,3 +11,175 @@
 # ******************************************************************************
 
 """Common Cartridge true/false item."""
+
+import json
+from xml.etree.ElementTree import Element as ETElement  # nosec B405
+from xml.etree.ElementTree import SubElement as ETSubElement  # nosec B405
+
+import defusedxml.ElementTree as ET
+
+from .item import Item
+
+
+class TrueFalse(Item):
+    """A Common Cartridge true/false item."""
+
+    def __init__(self, question, answer, **kwargs):
+        """Initialize a multiple choice item.
+
+        Parameters
+        ----------
+        question : str
+            The HTML formatted question text.
+        answer : bool
+            The Boolean answer.
+
+        Initialize a true/false item by storing the question and
+        answer data as plain Python objects for later output and as
+        IMSCC XML elements.
+        """
+        # Call the super.
+        super().__init__(**kwargs)
+
+        self.question = question
+        self.answer = answer
+
+        self.item = ETElement("item", ident=str(self.uuid))
+        self.itemmetadata = ETSubElement(self.item, "itemmetadata")
+        self.qtimetadata = ETSubElement(self.itemmetadata, "qtimetadata")
+        self.qtimetadatafield = ETSubElement(self.qtimetadata, "qtimetadatafield")
+        self.fieldlabel = ETSubElement(self.qtimetadatafield, "fieldlabel")
+        self.fieldlabel.text = "cc_profile"
+        self.fieldentry = ETSubElement(self.qtimetadatafield, "fieldentry")
+        self.fieldentry.text = "cc.true_false.v0p1"
+        self.presentation = ETSubElement(self.item, "presentation")
+        self.material = ETSubElement(self.presentation, "material")
+        self.mattext = ETSubElement(self.material, "mattext", texttype="text/html")
+        self.mattext.text = question
+
+        self.response = ETSubElement(
+            self.presentation,
+            "response_lid",
+            ident=str(self.uuid),
+            rcardinality="Single",
+        )
+        self.choices = ETSubElement(self.response, "render_choice")
+
+        correct = f"{self.uuid}-01" if answer else f"{self.uuid}-02"
+
+        # True.
+        ident = f"{self.uuid}-01"
+        choice = ETSubElement(self.choices, "response_label", ident=ident)
+        material = ETSubElement(choice, "material")
+        mattext = ETSubElement(material, "mattext", texttype="text/plain")
+        mattext.text = "True"
+
+        # False.
+        ident = f"{self.uuid}-02"
+        choice = ETSubElement(self.choices, "response_label", ident=ident)
+        material = ETSubElement(choice, "material")
+        mattext = ETSubElement(material, "mattext", texttype="text/plain")
+        mattext.text = "False"
+
+        grade = ETSubElement(self.item, "resprocessing")
+        outcomes = ETSubElement(grade, "outcomes")
+        ETSubElement(
+            outcomes,
+            "decvar",
+            maxvalue="100",
+            minvalue="0",
+            varname="SCORE",
+            vartype="Decimal",
+        )
+        cond = ETSubElement(
+            grade,
+            "respcondition",
+            attrib={
+                "continue": "No",
+            },
+        )
+        condvar = ETSubElement(cond, "conditionvar")
+        varequal = ETSubElement(condvar, "varequal", respident=str(self.uuid))
+        varequal.text = correct
+        setvar = ETSubElement(cond, "setvar", action="Set", varname="SCORE")
+        setvar.text = "100"
+
+        return
+
+    @classmethod
+    def from_json(cls, data, **kwargs):
+        """Instantiate a ``TrueFalse`` item from JSON data.
+
+        Instantiate a ``TrueFalse`` item from JSON data.
+
+        Parameters
+        ----------
+        cls
+            The ``TrueFalse`` class.
+        data : str
+            A string containing JSON data with which to generate the
+            item.
+        """
+        d = json.loads(data)
+
+        return cls(d["question"], d["answer"], **kwargs)
+
+    @classmethod
+    def from_xml(cls, item, **kwargs):
+        """Instantiate from IMSCC multiple choice item XML data.
+
+        Instantiate a ``TrueFalse`` item from IMSCC multiple choice
+        item XML data.
+
+        Parameters
+        ----------
+        cls
+            The ``MultipleChoice`` class.
+        item : str
+            A string containing IMSCC true/false XML data with which
+            to generate the item.
+        """
+        tree = ET.fromstring(item)
+
+        # Question text.
+        q = tree.find("presentation").find("material").find("mattext").text
+
+        # Correct answer.
+        correct = (
+            tree.find("resprocessing")
+            .find("respcondition")
+            .find("conditionvar")
+            .find("varequal")
+            .text
+        )
+
+        # Answer choices.
+        a = None
+        for ele in (
+            tree.find("presentation")
+            .find("response_lid")
+            .find("render_choice")
+            .findall("response_label")
+        ):
+            if correct == ele.get("ident"):
+                a = (
+                    True
+                    if ele.find("material").find("mattext").text.lower() == "true"
+                    else False
+                )
+                break
+
+        return cls(q, a, **kwargs)
+
+    def to_json(self):
+        """Create JSON string from item data."""
+        return json.dumps(
+            {
+                "question": str(self.question),
+                "answer": bool(self.answer),
+            }
+        )
+
+    def to_xml(self):
+        """Create XML string from item data."""
+        return ET.tostring(self.item, encoding="unicode", xml_declaration=True)
